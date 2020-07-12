c48940e57e48d516c3349ac793bd32d25002aeb2
omonkulov <somonkulov2018@gmail.com>
2020-07-11 22:24:38 -0400

    First version of start.sh

---
+++ start.sh
@@ -0,0 +1,39 @@
+echo -e "\n\n\nStarting"
+echo -e "-------- \n\n"
+
+echo    "Updating apt-get"
+echo    "------------------------------"
+sudo apt-get update
+echo    "Done"
+echo -e "------------------------------\n"
+
+echo    "Installing OpenJdk11"
+echo    "------------------------------"
+sudo apt-get install openjdk-11-jdk -y
+echo    "Done" 
+echo -e "------------------------------\n"
+
+echo    "Installing snapd"
+echo    "------------------------------"
+sudo apt-get install snapd  -y
+echo    "Done" 
+echo -e "------------------------------\n"
+
+echo    "Installing Eclipse"
+echo    "------------------------------"
+snap install --classic eclipse
+echo    "Done"
+echo -e "------------------------------\n"
+
+echo    "Installing Visual Studio Code"
+echo    "------------------------------"
+snap install code --classic
+echo    "Done"
+echo -e "------------------------------\n"
+
+echo    "Cleaning apt"
+echo    "------------------------------"
+sudo apt-get autoremove -y
+echo    "Done"
+echo -e "------------------------------\n"
+
